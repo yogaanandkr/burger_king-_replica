845ca6242e54369bb72a178c122b52d71c9ccc9b
yogaanandkr <yokeshanand762001@gmail.com>
2024-04-10 09:40:18 +0530

    folder structured

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React, { Fragment, StrictMode } from "react";
 // import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
-import Bk_nav from "./Bk_nav";
-import Poster from "./Poster";
-import Bkcards from "./Bkcards";
-import Download from "./Download";
-import Footer from "./Footer";
-import Menu from "./Menu";
+import Bk_nav from "./components/nav/Bk_nav";
+import Poster from "./components/poster/Poster";
+import Bkcards from "./components/cards/Bkcards";
+import Download from "./components/download/Download";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
 
 let root = ReactDOM.createRoot(document.getElementById("root"));
 
